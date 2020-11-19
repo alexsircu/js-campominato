184662f5179481c0936116f7f01c9e48e37b8849
Alex Sircu <alex.sricu@gmail.com>
2020-11-19 16:25:01 +0100

    Funzione per numeri duplicati

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// Descrizione
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// function randomFunction(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+//
+// var randomPCArray = [];
+// for (var i = 1; i <= 16; i++) {
+//   var generatedNumbersPC = randomFunction(1, 100);
+//   randomPCArray.push(generatedNumbersPC);
+//   if (randomPCArray[i] === randomPCArray.includes()) {
+//     alert("errore");
+//   }
+// }
+//
+// console.log(randomPCArray);
+
+function checkIfNumberAlreadyExist(number, array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] == number) {
+      return true;
+    }
+    return false;
+  }
+}
+
+var prova = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(prova);
+
+checkIfNumberAlreadyExist(1, prova)
+if (true) {
+  alert("errore");
+} else {
+  alert("Va bene");
+}
